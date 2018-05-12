810e00c16d6f0edc81c46459975557a904e2651a
Layon Martins <layonmartins03@gmail.com>
2018-05-12 10:16:49 -0300

    Class 05  - Functions

--- myScript.sh
+++ myScript.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 # My comment
-:<<"block"
-echo testing 1
-echo testing 2
-block
-echo "Hello, world!"
+#HELLO="Hello, world!"
+#echo $HELLO
+#world=("Shell Script" "Bash" "GNU" "Linux" "Debian")
+# echo ${#world[@]} #<-- print count number of elements the array
+# echo ${world[*]} #<-- print all elements of the array
+#echo ${world[@]:2} #<-- print all ellements from second element
+
+MyFunction(){
+	local Hello="Hello, world!"
+	#echo "I write in $2 $1"
+	echo "I have passed $# parameters"
+	echo $Hello
+	return
+	echo "Other thing"
+}
+
+#MyFunction $@
+
+#declare -r MyConstant="This is constant" #<-- Always have the same value
+
+#echo $MyConstant
+
+#Access directories
+cd MyDirectory/
+ls
